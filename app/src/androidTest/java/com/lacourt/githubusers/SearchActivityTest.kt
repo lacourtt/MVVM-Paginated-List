757b1fe52e5a1ef6217809b9e3f27fab296bb855
Igor Lacourt <igorlacourt@gmail.com>
2023-05-15 19:10:38 -0300

    implement ui tests

--- app/src/androidTest/java/com/lacourt/githubusers/SearchActivityTest.kt
+++ app/src/androidTest/java/com/lacourt/githubusers/SearchActivityTest.kt
@@ -2,41 +2,39 @@ package com.lacourt.githubusers
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.lacourt.githubusers.paging.UserListPageAdapter
-import com.lacourt.githubusers.view.MainActivity
+import com.lacourt.githubusers.view.SearchActivity
 import org.junit.Rule
 import org.junit.Test
 
-class MainActivityTest {
+class SearchActivityTest {
     @get:Rule
-    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+    val activityRule = ActivityScenarioRule(SearchActivity::class.java)
 
     @Test
     fun testRecyclerViewItemClick() {
+        // Simulate writing a text in the search bar
+        onView(withId(R.id.et_search))
+            .perform(typeText("mojombo"))
+
+        // Simulate click on the search button
+        onView(withId(R.id.iv_search_icon)).perform(click())
+        Thread.sleep(2000)
+
         // Simulate click on a specific item in the RecyclerView
         val position = 0
-        onView(withId(R.id.rv_users_list))
+        onView(withId(R.id.rv_searched_list))
             .perform(actionOnItemAtPosition<UserListPageAdapter.UserListViewHolder>(position, click()))
-        Thread.sleep(2000)
-        
-        // Verify that the UserDetailsActivity is launched with the expected intent extras
+
+        // Verify that the UserDetailsActivity is launched
         onView(withId(R.id.root_user_details))
             .check(matches(isDisplayed()))
     }
 
-    @Test
-    fun testSearchButtonClicked() {
-        // Simulate click on the search button
-        onView(withId(R.id.tv_search_users)).perform(click())
-        Thread.sleep(2000)
-
-        // Verify that the SearchActivity is launched
-        onView(withId(R.id.root_search_activity))
-            .check(matches(isDisplayed()))
-    }
 }
